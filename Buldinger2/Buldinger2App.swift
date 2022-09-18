629d06a679ff91e8cf4ba3ae771e8bb2d053ef2e
Sayed H. Mortazavi <h.mortazavi@ibims.org>
2022-09-18 08:08:04 +0200

    Initial Commit

---
+++ Buldinger2/Buldinger2App.swift
@@ -0,0 +1,17 @@
+//
+//  Buldinger2App.swift
+//  Buldinger2
+//
+//  Created by Hadi Mortazavi on 18.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Buldinger2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
